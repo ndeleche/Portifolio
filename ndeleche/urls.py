0c6bc3941b6e85590de55e08c773b8b462bcceac
i.a.m.ndeleche <ndelechehamis@gmail.com>
2023-11-17 12:28:03 -0800

    part-2

--- ndeleche/urls.py
+++ ndeleche/urls.py
@@ -1,3 +1,8 @@
-from django.conf import settings
-from django.contrib import admin
 from django.urls import path
+from .import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+
+]
--- ndeleche/views.py
+++ ndeleche/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+# home
+
+
+def index(request):
+    return render(request, 'index.html')
