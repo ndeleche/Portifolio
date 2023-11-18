46f480b4593aa81ddc1823fdba2a57a992ef6c49
i.a.m.ndeleche <ndelechehamis@gmail.com>
2023-11-18 06:24:31 -0800

    part-3

--- ndeleche/views.py
+++ ndeleche/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
+from .models import Home, About, Profile, Category, Skills, Portifolio
 
-# home
 
+# Index page.
 
 def index(request):
-    return render(request, 'index.html')
+
+    # home
+    home = Home.objects.latest('updated')
+
+    # about
+    about = About.objects.latest('updated')
+    profiles = Profile.objects.filter(about=about)
+
+    # skills
+    categories = Category.objects.all()
+
+    # portifolio
+    portifolios = Portifolio.objects.all()
+
+    context = {
+        'home': home,
+        'about': about,
+        'profiles': profiles,
+        'categories': categories,
+        'portifolios': portifolios
+
+    }
+
+    return render(request, 'index.html', context)
