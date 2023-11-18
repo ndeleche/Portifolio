1665bac7600054ab043302aaaa9c8aac1b0d8aeb
i.a.m.ndeleche <ndelechehamis@gmail.com>
2023-11-18 12:59:00 -0800

    new version

--- portifolio/settings.py
+++ portifolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3i#ewb*1o2o=c#2k%sxatars!qh=vu^5e252r%)7_wnai+edr8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
