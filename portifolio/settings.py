fee347b6074ca84f92918e5349a67f1c75020303
i.a.m.ndeleche <ndelechehamis@gmail.com>
2023-12-18 22:53:10 -0800

    new versin of portifolio website

--- portifolio/settings.py
+++ portifolio/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ndeleche',
+    'whitenoise',
 ]
 
 MIDDLEWARE = [
